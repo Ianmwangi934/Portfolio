cc3a0c017c892a4bcab36d04ba8969c74ee98a24
ian mwangi <ian_mwangi@localhost.localdomain>
2025-07-06 10:47:26 +0300

    Updtated my about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,22 @@ const About = () =>{
         <section className="about" id="about">
         <h2>About me</h2>
         <p>
-            I'm <strong>Ian Murigu</strong>, a passionate Web Developer and Cybersecurity Enthusiast based in Kenya.
-            I love building full-stack appliactions using technologies like <strong>Django</strong>, <strong>React</strong>
-            and <strong>Rest APIs</strong>
+        I'm <strong>Ian Murigu</strong>, a passionate <strong>Full-Stack Web Developer</strong> and <strong>Cybersecurity Enthusiast</strong> based in Kenya.
+        I specialize in building scalable, secure web applications using technologies like <strong>Django</strong>, <strong>React</strong>, and <strong>REST APIs</strong>.
+        My background working in customer support coordination in Dubai taught me the value of clear communication, problem-solving, 
+        and user-focused design.
+
+
         </p>
         <p>
-            My background in tech support and security helps me build secure, interactive, Scalable applications. I am passionate
-            about continous growth and when am not coding I enjoy reading psychological and business books like 
-            <em>The Psychology of Money</em> and <em>Rich Dad Poor Dad</em>.
+        I began my tech journey studying Cybersecurity and Digital Forensics at Meru University of Science and Technology, and 
+        while the course is currently incomplete, it laid a strong foundation in secure systems and digital awareness. 
+        I've since continued to learn independently and build real-world projects.
         </p>
         <p>
-            I'm currently open to freelance, remote Hybrid opportunities - especially those that let me build real-world solutions and
-            grow as a developer and entrepreneur.
+        Outside of tech, I enjoy reading psychology and business books like <em>The Psychology of Money</em> and <em>Rich Dad Poor Dad</em>, which inspire me to grow personally 
+        and professionally.I'm currently open to freelance, remote, or hybrid opportunities — especially roles that let me create impactful solutions
+        and grow as both a developer and entrepreneur.
         </p>
 
     </section>

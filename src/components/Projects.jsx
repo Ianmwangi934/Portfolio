4ee09f9e93a2d3cbc34e09b6ab16d20341668e5b
ian mwangi <ian_mwangi@localhost.localdomain>
2025-08-18 13:11:01 +0300

    updated github links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const projects = [
     image: "/project-thumbnails/ecommerce.png",
     links: {
       live: "",     // Replace with actual link
-      github: "https://github.com/Ianmwangi934/Fashion_market",
+      github: "https://github.com/Ianmwangi934/Fashion_marketplace",
       demo: "https://youtu.be/TAi_iLruJwM?si=1yjKnZQ5dYLcP2cV"
     }
   },
